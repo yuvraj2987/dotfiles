d8b5c7ad487c27615f24081caeb5e5037e2e0815
Amit Kulkarni <yuvraj2987@gmail.com>
2024-11-06 18:00:23 -0800

    Changed env.sh to template file.
    Split linux env vars and macos env vars.
    Removed go, ruby and java settings.
    These are not in use for now.

---
+++ dotfiles/env-profiles/01_macos_vars.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# Path
+## Homebrew
+export PATH="/opt/homebrew/bin:$PATH"
+
+## Add GnuSed to MacOS
+export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
---
+++ dotfiles/env-profiles/06_wepay_vars.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env zsh
+
+# PATH
+## Wetools
+export PATH="$PATH:/Users/amitk/we-tools-cli/bin"
+
+## Google SDK
+export PATH="$PATH:/Users/amitk/google-cloud-sdk/bin"
+
