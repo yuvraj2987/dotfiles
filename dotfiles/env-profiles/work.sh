0db8c61ecd2068fe5738d8d109d92d78e87de142
Amit Kulkarni <yuvraj2987@gmail.com>
2025-09-17 11:45:52 -0700

    Added hashi binary to PATH.

--- dotfiles/env-profiles/work.sh
+++ dotfiles/env-profiles/work.sh
@@ -5,5 +5,13 @@
 export GOPATH=$HOME/work/rbx/go
 export GOBIN=$GOPATH/bin/
 
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# Hashi tools
+export PATH="$PATH:$HOME/work/rbx/tools"
+
 
 
