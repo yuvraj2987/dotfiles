de022fba6632f589462a278fce83fbcf122c8b0b
Amit Kulkarni <4722567+yuvraj2987@users.noreply.github.com>
2026-04-15 14:22:13 -0700

    Removed rbx/tools from PATH
    Standarizing custom binary path to ~/.local

--- dotfiles/env-profiles/work.sh
+++ dotfiles/env-profiles/work.sh
@@ -7,17 +7,12 @@ PATH=/opt/chefdk/embedded/bin:$PATH
 PATH=$PATH:/Users/${USER}/.chefdk/gem/ruby/${GEM_VERSION}/bin
 
 
-# Go
-export GOPATH=$HOME/work/rbx/go
-export GOBIN=$GOPATH/bin/
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-# Hashi tools
-export PATH="$PATH:$HOME/work/rbx/tools"
 
 
 
